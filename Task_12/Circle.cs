2a6d2942fb9939638e1e999cfddd6daf0fbbefa7
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-03 15:23:30 +0300

    Добавьте файлы проекта.

---
+++ Task_12/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static public double GetSquare(double r)
+        {
+            return Math.PI * (Math.Pow(r,2));
+        }
+
+
+        static public double GetAffiliation(double x, double y, double x0, double y0)
+        {
+
+            return Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2);
+
+        }
+
+
+    }
+}
---
+++ Task_12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса круга");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координаты x точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координаты y точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координаты x0 центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение координаты y0 центра окружности");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            double length = Circle.GetLength(r);
+            double square = Circle.GetSquare(r);
+            
+            
+            
+            double affiliation = Circle.GetAffiliation(x, y, x0, y0);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Длина окружности равна " + length);
+            Console.WriteLine("Площадь окружности равна " + square);
+            Console.WriteLine(affiliation);
+            Console.WriteLine((Math.Pow(r, 2)) == affiliation ? "Точка принадлежит окружности" : "Точка НЕ принадлежит окружности");
+            Console.ReadLine();
+
+        }
+    }
+}
